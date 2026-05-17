2dd76ccfb2f5aa04083b30a0d925f6bbbfc2d026
Bruno  de Paula Fernandes <BrunoFernandes2315@outlook.com>
2026-05-17 00:48:15 -0300

    criação do projeto

---
+++ src/main/java/edu/apiFuncionarios/ApiFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package edu.apiFuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFuncionariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/apiFuncionarios/ApiFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.apiFuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
